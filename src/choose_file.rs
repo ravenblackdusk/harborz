94048db146e358734ff833843f64934714df68cb
dusk <ravenblackdusk@protonmail.com>
2023-06-15 12:11:07 +0330

    Cleanup

--- src/choose_file.rs
+++ src/choose_file.rs
@@ -1,7 +1,7 @@
 #![allow(deprecated)]
 
 use anyhow::{anyhow, Result};
-use diesel::{ExpressionMethods, insert_or_ignore_into, RunQueryDsl};
+use diesel::{ExpressionMethods, insert_or_ignore_into, QueryDsl, RunQueryDsl};
 use diesel::result::Error;
 use gtk::*;
 use prelude::*;
@@ -10,28 +10,30 @@ use glib::{clone, MainContext};
 use FileChooserAction::SelectFolder;
 use crate::db::get_connection;
 use crate::models::Collection;
-use crate::Removable;
+use crate::CollectionGrid;
 use crate::schema::collections::dsl::collections;
 use crate::schema::collections::path;
 
-fn add_directory_to_collection(dialog: &FileChooserDialog, collection_box: &Box) -> Result<()> {
+fn add_directory_to_collection(dialog: &FileChooserDialog, collection_grid: &Grid) -> Result<()> {
     dialog.files().iter::<File>().map(|file| { Some(file.ok()?.path()?.to_str()?.to_owned()) })
         .collect::<Option<Vec<_>>>().ok_or(anyhow!("error trying to get paths"))?.iter().filter_map(|path_string| {
         match insert_or_ignore_into(collections).values(path.eq(path_string)).get_result::<Collection>(&mut get_connection()) {
             Err(Error::NotFound) => None,
             result => Some(result),
         }
-    }).map(|result| { Ok(collection_box.append_collection_remove(&result?)) }).collect::<Result<Vec<_>>>()?;
+    }).map(|result| {
+        Ok(collection_grid.add(&result?, collections.count().first::<i64>(&mut get_connection())? as i32))
+    }).collect::<Result<Vec<_>>>()?;
     Ok(())
 }
 
-pub fn choose_file(collection_box: &Box) {
+pub fn choose_file(collection_grid: &Grid) {
     let dialog = FileChooserDialog::builder().title("choose collection directories")
         .action(SelectFolder).select_multiple(true).build();
     dialog.add_buttons(&[("cancel", ResponseType::Cancel), ("choose", ResponseType::Ok)]);
-    MainContext::default().spawn_local(clone!(@weak dialog, @weak collection_box => async move {
+    MainContext::default().spawn_local(clone!(@weak dialog, @weak collection_grid => async move {
         if dialog.run_future().await == ResponseType::Ok {
-            add_directory_to_collection(&dialog, &collection_box).expect("should be able to add directories to collection");
+            add_directory_to_collection(&dialog, &collection_grid).expect("should be able to add directories to collection");
         }
         dialog.close();
     }));
--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,29 @@ mod db;
 use diesel::{migration, delete, QueryDsl, RunQueryDsl};
 use migration::Result;
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
-use gtk::{prelude, glib, Orientation, Button, Label, Application, Grid, ApplicationWindow};
+use gtk::{prelude, glib, Button, Label, Application, Grid, ApplicationWindow};
 use prelude::*;
 use glib::{clone, ExitCode};
-use Orientation::*;
 use crate::choose_file::choose_file;
 use crate::db::get_connection;
 use crate::models::Collection;
 use crate::schema::collections::dsl::collections;
 
-trait Removable {
-    fn append_collection_remove(&self, collection: &Collection);
+trait CollectionGrid {
+    fn add(&self, collection: &Collection, row: i32);
 }
 
-impl Removable for gtk::Box {
-    fn append_collection_remove(&self, collection: &Collection) {
-        let collection_remove = gtk::Box::builder().orientation(Horizontal).build();
+impl CollectionGrid for Grid {
+    fn add(&self, collection: &Collection, row: i32) {
         let remove_button = Button::builder().label("-").build();
         let id = collection.id;
-        remove_button.connect_clicked(clone!(@weak self as borrowed_self, @weak collection_remove => move |_| {
+        remove_button.connect_clicked(clone!(@weak self as borrowed_self => move |_| {
             delete(collections.find(id)).execute(&mut get_connection())
                 .expect("should be able to delete collection");
-            borrowed_self.remove(&collection_remove);
+            borrowed_self.remove_row(row);
         }));
-        collection_remove.append(&Label::builder().label(&collection.path).build());
-        collection_remove.append(&remove_button);
-        self.append(&collection_remove);
+        self.attach(&Label::builder().label(&collection.path).build(), 0, row, 1, 1);
+        self.attach(&remove_button, 1, row, 1, 1);
     }
 }
 
@@ -42,14 +39,16 @@ fn main() -> Result<ExitCode> {
     get_connection().run_pending_migrations(MIGRATIONS)?;
     let application = Application::builder().application_id("eu.agoor.music-player").build();
     application.connect_activate(|application| {
-        let collection_box = gtk::Box::builder().orientation(Vertical).build();
-        for collection in collections.load::<Collection>(&mut get_connection()).expect("should be able to get collections") {
-            collection_box.append_collection_remove(&collection);
+        let collection_grid = Grid::builder().row_spacing(4).column_spacing(4).build();
+        for (i, collection) in collections.load::<Collection>(&mut get_connection())
+            .expect("should be able to get collections").into_iter().enumerate() {
+            collection_grid.add(&collection, i as i32);
         }
         let browse_button = Button::builder().label("browse").build();
-        browse_button.connect_clicked(clone!(@weak collection_box => move |_| { choose_file(&collection_box); }));
-        let grid = Grid::builder().build();
-        grid.attach(&collection_box, 0, 0, 1, 1);
+        browse_button.connect_clicked(clone!(@weak collection_grid => move |_| { choose_file(&collection_grid); }));
+        let grid = Grid::builder().row_spacing(4).column_spacing(4).margin_start(4).margin_end(4)
+            .margin_bottom(4).margin_top(4).build();
+        grid.attach(&collection_grid, 0, 0, 1, 1);
         grid.attach(&browse_button, 0, 1, 1, 1);
         ApplicationWindow::builder().application(application).title("music player").child(&grid)
             .build().present();
