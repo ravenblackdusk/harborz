4731b64e30a574854783b62d5003ce8079c6fad2
dusk <ravenblackdusk@protonmail.com>
2023-06-15 11:35:37 +0330

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ mod schema;
 mod models;
 mod db;
 
-use diesel::{delete, QueryDsl, RunQueryDsl};
+use diesel::{migration, delete, QueryDsl, RunQueryDsl};
+use migration::Result;
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
-use gtk::*;
+use gtk::{prelude, glib, Orientation, Button, Label, Application, Grid, ApplicationWindow};
 use prelude::*;
-use traits::{BoxExt, ButtonExt, GridExt};
-use Orientation::Vertical;
-use glib::clone;
-use gtk::Orientation::Horizontal;
+use glib::{clone, ExitCode};
+use Orientation::*;
 use crate::choose_file::choose_file;
 use crate::db::get_connection;
 use crate::models::Collection;
@@ -20,9 +19,9 @@ trait Removable {
     fn append_collection_remove(&self, collection: &Collection);
 }
 
-impl Removable for Box {
+impl Removable for gtk::Box {
     fn append_collection_remove(&self, collection: &Collection) {
-        let collection_remove = Box::builder().orientation(Horizontal).build();
+        let collection_remove = gtk::Box::builder().orientation(Horizontal).build();
         let remove_button = Button::builder().label("-").build();
         let id = collection.id;
         remove_button.connect_clicked(clone!(@weak self as borrowed_self, @weak collection_remove => move |_| {
@@ -38,12 +37,12 @@ impl Removable for Box {
 
 const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
-fn main() {
+fn main() -> Result<ExitCode> {
     std_logger::Config::logfmt().init();
-    get_connection().run_pending_migrations(MIGRATIONS).expect("should run pending migrations");
+    get_connection().run_pending_migrations(MIGRATIONS)?;
     let application = Application::builder().application_id("eu.agoor.music-player").build();
     application.connect_activate(|application| {
-        let collection_box = Box::builder().orientation(Vertical).build();
+        let collection_box = gtk::Box::builder().orientation(Vertical).build();
         for collection in collections.load::<Collection>(&mut get_connection()).expect("should be able to get collections") {
             collection_box.append_collection_remove(&collection);
         }
@@ -55,5 +54,5 @@ fn main() {
         ApplicationWindow::builder().application(application).title("music player").child(&grid)
             .build().present();
     });
-    application.run();
+    Ok(application.run())
 }
