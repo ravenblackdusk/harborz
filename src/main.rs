15735c886232e8f95cd7f1902b434f8885495b13
ghasemi <a.ghasemi@kian.digital>
2023-06-21 17:33:24 +0330

    Separate collection and home

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod controls;
 mod common;
 mod config;
 
+use std::rc::Rc;
 use diesel::migration;
 use migration::Result;
 use diesel_migrations::MigrationHarness;
@@ -14,6 +15,7 @@ use glib::ExitCode;
 use gtk::Orientation::Vertical;
 use db::MIGRATIONS;
 use crate::common::gtk_box;
+use crate::controls::media_controls;
 use crate::db::get_connection;
 
 fn main() -> Result<ExitCode> {
@@ -21,18 +23,26 @@ fn main() -> Result<ExitCode> {
     get_connection().run_pending_migrations(MIGRATIONS)?;
     let application = Application::builder().application_id("eu.agoor.music-player").build();
     application.connect_activate(|application| {
-        let main_box = gtk_box(Vertical);
-        main_box.append(&collection::frame());
-        main_box.append(&controls::media_controls());
+        let frame1 = Box::new(collection::frame());
+        let frame2 = Box::new(media_controls());
         let bar = HeaderBar::builder().title_widget(&Label::builder().label("music player").build()).build();
-        let menu = gtk_box(Vertical);
+        let window = Rc::new(ApplicationWindow::builder().application(application).child(&*frame2).titlebar(&bar).build());
         let collection_button = Button::builder().label("Collection").build();
+        collection_button.connect_clicked({
+            let window = window.clone();
+            move |_| { window.set_child(Some(&*frame1)); }
+        });
+        let menu = gtk_box(Vertical);
         menu.append(&collection_button);
-        let back = Button::builder().icon_name("go-home").build();
-        bar.pack_start(&back);
+        let home = Button::builder().icon_name("go-home").build();
+        home.connect_clicked({
+            let window = window.clone();
+            move |_| { window.set_child(Some(&*frame2)); }
+        });
+        bar.pack_start(&home);
         bar.pack_end(&MenuButton::builder().icon_name("open-menu-symbolic")
             .popover(&Popover::builder().child(&menu).build()).build());
-        ApplicationWindow::builder().application(application).child(&main_box).titlebar(&bar).build().present();
+        window.present();
     });
     Ok(application.run())
 }
