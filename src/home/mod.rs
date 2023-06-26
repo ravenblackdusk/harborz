a9c4d4220b41f33c9c265ab54c00547b6afcaf01
ghasemi <a.ghasemi@kian.digital>
2023-06-26 13:31:50 +0330

    Cleanup

--- src/home/mod.rs
+++ src/home/mod.rs
@@ -7,47 +7,47 @@ use crate::db::get_connection;
 use crate::schema::songs::{album, artist};
 use crate::schema::songs::dsl::songs;
 
-const ARTIST: &'static str = "artist";
-const ALBUM: &'static str = "album";
-const SONG: &'static str = "song";
+const ID: &'static str = "id";
 
-fn list_box(strings: Vec<Option<String>>, data_key: &str) -> ListBox {
+fn list_box<T: 'static, S: Fn(&T) -> &str, F: Fn(&T) + 'static>(row_items: Vec<T>, to_str: S, on_row_activated: F) -> ListBox {
     let list_box = ListBox::builder().selection_mode(SelectionMode::None).build();
-    for string in strings {
-        let deref_string = string.as_deref();
-        let label = Label::builder().label(deref_string.unwrap_or("None")).build();
-        unsafe { label.set_data(data_key, deref_string.map(|it| { it.to_owned() })); }
+    for row_item in row_items {
+        let label = Label::builder().label(to_str(&row_item)).build();
+        unsafe { label.set_data(ID, row_item); }
         list_box.append(&label);
     }
+    list_box.connect_row_activated(move |_, list_box_row| unsafe {
+        let item = gtk::prelude::ListBoxRowExt::child(list_box_row).unwrap().data::<T>(ID).unwrap().as_ref();
+        on_row_activated(item);
+    });
     list_box
 }
 
+fn or_none(string: &Option<String>) -> &str {
+    string.as_deref().unwrap_or("None")
+}
+
 pub fn home() -> Rc<Frame> {
-    let artists = list_box(songs.select(artist).group_by(artist).get_results::<Option<String>>(&mut get_connection())
-        .unwrap(), ARTIST);
-    let frame = Rc::new(Frame::builder().child(&artists).build());
-    artists.connect_row_activated({
-        let frame = frame.clone();
-        move |_, artist_row| unsafe {
-            let artist_string = gtk::prelude::ListBoxRowExt::child(artist_row).unwrap().data::<Option<String>>(ARTIST)
-                .unwrap().as_ref();
-            let albums = list_box(songs.filter(artist.eq(artist_string)).select(album).group_by(album)
-                .get_results::<Option<String>>(&mut get_connection()).unwrap(), ALBUM);
-            albums.connect_row_activated({
-                let artist_string = artist_string.to_owned();
-                let frame = frame.clone();
-                move |_, album_row| {
-                    let album_string = gtk::prelude::ListBoxRowExt::child(album_row).unwrap()
-                        .data::<Option<String>>(ALBUM).unwrap().as_ref();
-                    let song_vec = songs.filter(artist.eq(artist_string.to_owned()).and(album.eq(album_string)))
-                        .get_results::<Song>(&mut get_connection()).unwrap();
-                    let song_list_box = list_box(song_vec.into_iter().map(|song| { song.title.or(Some(song.path)) })
-                        .collect::<Vec<_>>(), SONG);
-                    frame.set_child(Some(&song_list_box));
-                }
-            });
-            frame.set_child(Some(&albums));
-        }
-    });
+    let frame = Rc::new(Frame::builder().build());
+    let artists = list_box(songs.select(artist).group_by(artist).get_results::<Option<String>>(&mut get_connection()).unwrap(),
+        or_none, {
+            let frame = frame.clone();
+            move |artist_string| {
+                let albums = list_box(songs.filter(artist.eq(artist_string)).select(album).group_by(album)
+                    .get_results::<Option<String>>(&mut get_connection()).unwrap(), or_none, {
+                    let artist_string = artist_string.to_owned();
+                    let frame = frame.clone();
+                    move |album_string| {
+                        let song_list_box = list_box(songs.filter(artist.eq(&artist_string).and(album.eq(album_string)))
+                            .get_results::<Song>(&mut get_connection()).unwrap(), |song_item| {
+                            song_item.title.as_deref().unwrap_or(song_item.path.as_str())
+                        }, move |song_item| {});
+                        frame.set_child(Some(&song_list_box));
+                    }
+                });
+                frame.set_child(Some(&albums));
+            }
+        });
+    frame.set_child(Some(&artists));
     frame
 }
