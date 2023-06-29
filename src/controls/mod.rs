386f236898ca90198f3327c324d0220676170203
dusk <ravenblackdusk@protonmail.com>
2023-06-29 12:43:20 +0330

    Play using playbin

--- src/controls/mod.rs
+++ src/controls/mod.rs
@@ -4,8 +4,11 @@ use std::path::PathBuf;
 use std::rc::Rc;
 use std::time::Duration;
 use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl, update};
+use gstreamer::{ClockTime, ElementFactory, SeekFlags};
+use gstreamer::prelude::{ElementExt, ElementExtManual, ObjectExt};
+use gstreamer::State::{Null, Paused, Playing};
 use gtk::*;
-use gtk::prelude::*;
+use gtk::prelude::{BoxExt, ButtonExt, RangeExt, WidgetExt};
 use Orientation::Horizontal;
 use crate::common::gtk_box;
 use crate::common::util::PathString;
@@ -33,8 +36,8 @@ impl PlayPause {
     }
 }
 
-fn format(timestamp: i64) -> String {
-    let seconds = Duration::from_micros(timestamp as u64).as_secs();
+fn format(timestamp: u64) -> String {
+    let seconds = Duration::from_nanos(timestamp).as_secs();
     format!("{}:{:02}", seconds / 60, seconds % 60)
 }
 
@@ -43,47 +46,38 @@ pub fn media_controls() -> Wrapper {
         .get_result::<(String, String)>(&mut get_connection()).map(|(collection_path, current_song_path)| {
         collection_path.to_path().join(current_song_path.to_path())
     }).unwrap_or(PathBuf::from(""));
-    let media_file = Rc::new(MediaFile::for_filename(path_buf));
+    let playbin = ElementFactory::make("playbin").build().unwrap();
+    let string = format!("file:{}", path_buf.to_str().unwrap());
+    playbin.set_property("uri", string);
+    playbin.set_state(Paused).unwrap();
     let (icon, tooltip) = PlayPause::Play.icon_tooltip();
     let play_pause = Button::builder().icon_name(icon).tooltip_text(tooltip).build();
-    let time = Label::builder().label(format(0)).build();
+    let position = Label::new(playbin.query_position::<ClockTime>().map(|a| { format(a.nseconds()) }).as_deref());
     let scale = Rc::new(Scale::builder().hexpand(true).build());
-    let duration_label = Rc::new(Label::builder().build());
+    let duration = playbin.query_duration().map(ClockTime::nseconds);
+    if let Some(duration) = duration {
+        scale.set_range(0.0, duration as f64);
+    }
+    let duration_label = Rc::new(Label::new(duration.map(format).as_deref()));
     let gtk_box = gtk_box(Horizontal);
     gtk_box.append(&Button::builder().icon_name("media-skip-backward").tooltip_text("Previous").build());
     gtk_box.append(&play_pause);
     gtk_box.append(&Button::builder().icon_name("media-skip-forward").tooltip_text("Next").build());
-    gtk_box.append(&time);
+    gtk_box.append(&position);
     gtk_box.append(&*scale);
     gtk_box.append(&*duration_label);
     gtk_box.append(&*volume_button({
-        let media_file = media_file.clone();
-        move |volume| { media_file.set_volume(volume); }
+        let playbin = playbin.clone();
+        move |volume| { playbin.set_property("volume", volume); }
     }));
-    media_file.connect_duration_notify({
-        let scale = scale.clone();
-        move |media_file| {
-            let duration = media_file.duration();
-            duration_label.set_label(&format(duration));
-            scale.set_range(0.0, duration as f64);
-        }
-    });
-    media_file.connect_timestamp_notify({
-        let scale = scale.clone();
-        move |media_file| {
-            let timestamp = media_file.timestamp();
-            time.set_label(&format(timestamp));
-            scale.set_value(timestamp as f64);
-        }
-    });
     play_pause.connect_clicked({
-        let media_file = media_file.clone();
+        let playbin = playbin.clone();
         move |play_pause| {
-            let (icon, tooltip) = if media_file.is_playing() {
-                media_file.pause();
+            let (icon, tooltip) = if playbin.current_state() == Playing {
+                playbin.set_state(Paused).unwrap();
                 PlayPause::Play
             } else {
-                media_file.play();
+                playbin.set_state(Playing).unwrap();
                 PlayPause::Pause
             }.icon_tooltip();
             play_pause.set_icon_name(icon);
@@ -91,10 +85,11 @@ pub fn media_controls() -> Wrapper {
         }
     });
     scale.connect_change_value({
-        let media_file = media_file.clone();
+        let playbin = playbin.clone();
         move |_, scroll_type, value| {
             if scroll_type == ScrollType::Jump {
-                media_file.seek(value as i64);
+                playbin.seek_simple(SeekFlags::FLUSH | SeekFlags::KEY_UNIT, ClockTime::from_nseconds(value as u64))
+                    .unwrap();
             }
             Inhibit(true)
         }
@@ -103,11 +98,11 @@ pub fn media_controls() -> Wrapper {
     wrapper.connect_local(SONG_SELECTED, true, move |params| {
         if let [_, song_id, current_song_path, collection_path] = &params {
             update(config).set(current_song_id.eq(song_id.get::<i32>().unwrap())).execute(&mut get_connection()).unwrap();
-            let playing = media_file.is_playing();
-            if playing { play_pause.emit_clicked(); }
-            media_file.set_filename(Some(current_song_path.get::<String>().unwrap().to_path()
-                .join(collection_path.get::<String>().unwrap().to_path())));
-            // if playing { media_file.play(); }
+            let playing = playbin.current_state() == Playing;
+            if playing { playbin.set_state(Null).unwrap(); }
+            playbin.set_property("uri", format!("file:{}", collection_path.get::<String>().unwrap().to_path()
+                .join(current_song_path.get::<String>().unwrap().to_path()).to_str().unwrap()));
+            if playing { playbin.set_state(Playing).unwrap(); } else { play_pause.emit_clicked(); }
         }
         None
     });
