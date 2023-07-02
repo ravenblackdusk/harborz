93759c32fd724b437a18b92fb226dd6d0ea00be7
ghasemi <a.ghasemi@kian.digital>
2023-07-02 18:03:35 +0330

    Use column view

--- src/home/mod.rs
+++ src/home/mod.rs
@@ -1,11 +1,14 @@
+use std::ops::Deref;
+use std::rc::Rc;
 use diesel::{BoolExpressionMethods, ExpressionMethods, QueryDsl, RunQueryDsl};
-use gtk::{Label, ListBox, ScrolledWindow, SelectionMode, Widget};
-use gtk::Orientation::Horizontal;
-use gtk::prelude::{BoxExt, IsA, ObjectExt};
+use gtk::{ColumnView, ColumnViewColumn, Label, ListItem, NoSelection, ScrolledWindow, SignalListItemFactory, Widget};
+use gtk::prelude::{Cast, CastNone, IsA, ObjectExt, StaticType, WidgetExt};
 use gtk::Align::Start;
+use gtk::gio::ListStore;
+use gtk::glib::BoxedAnyObject;
+use gtk::pango::EllipsizeMode;
 use crate::collection::model::Collection;
 use crate::collection::song::Song;
-use crate::common::gtk_box;
 use crate::common::util::{format, PathString};
 use crate::common::wrapper::{SONG_SELECTED, Wrapper};
 use crate::db::get_connection;
@@ -13,52 +16,72 @@ use crate::schema::collections::dsl::collections;
 use crate::schema::songs::{album, artist, track_number};
 use crate::schema::songs::dsl::songs;
 
-const ID: &'static str = "id";
-
-fn list_box<T: 'static, W: IsA<Widget>, S: Fn(&T) -> W, F: Fn(&T) + 'static>(scrolled_window: &ScrolledWindow,
-    row_items: Vec<T>, to_widget: S, on_row_activated: F) {
-    let list_box = ListBox::builder().selection_mode(SelectionMode::None).build();
-    for row_item in row_items {
-        let widget = to_widget(&row_item);
-        unsafe { widget.set_data(ID, row_item); }
-        list_box.append(&widget);
+fn list_box<T: 'static, W: IsA<Widget>, S: Fn(Rc<T>) -> W + ?Sized + 'static, F: Fn(Rc<T>) + 'static>(
+    scrolled_window: &ScrolledWindow, row_items: Vec<Rc<T>>, columns: Vec<(Box<S>, bool)>, on_row_activated: F) {
+    let store = ListStore::new(BoxedAnyObject::static_type());
+    for row_item in row_items.iter() {
+        store.append(&BoxedAnyObject::new(row_item.clone()));
     }
-    list_box.connect_row_activated(move |_, list_box_row| {
-        let item = unsafe { gtk::prelude::ListBoxRowExt::child(list_box_row).unwrap().data::<T>(ID).unwrap().as_ref() };
-        on_row_activated(item);
-    });
-    scrolled_window.set_child(Some(&list_box));
+    let column_view = ColumnView::builder().single_click_activate(true).model(&NoSelection::new(Some(store))).build();
+    column_view.first_child().unwrap().set_visible(false);
+    column_view.connect_activate(move |_, row| { on_row_activated(row_items[row as usize].clone()); });
+    for (to_widget, expand) in columns {
+        let item_factory = SignalListItemFactory::new();
+        item_factory.connect_bind(move |_, item| {
+            let list_item = item.downcast_ref::<ListItem>().unwrap();
+            list_item.set_selectable(false);
+            list_item.set_child(Some(&to_widget(
+                list_item.item().and_downcast::<BoxedAnyObject>().unwrap().borrow::<Rc<T>>().deref().clone()
+            )));
+        });
+        column_view.append_column(&ColumnViewColumn::builder().factory(&item_factory).expand(expand).build());
+    }
+    scrolled_window.set_child(Some(&column_view));
 }
 
-fn or_none(string: &Option<String>) -> Label {
+fn or_none(string: Rc<Option<String>>) -> Label {
     Label::builder().label(string.as_deref().unwrap_or("None")).halign(Start).build()
 }
 
 pub fn set_body(scrolled_window: &ScrolledWindow, media_controls: &Wrapper) {
     list_box(scrolled_window,
-        songs.select(artist).group_by(artist).get_results::<Option<String>>(&mut get_connection()).unwrap(), or_none, {
+        songs.select(artist).group_by(artist).get_results::<Option<String>>(&mut get_connection()).unwrap()
+            .into_iter().map(Rc::new).collect::<Vec<_>>(),
+        vec![(Box::new(or_none), true)], {
             let scrolled_window = scrolled_window.clone();
             let media_controls = media_controls.clone();
             move |artist_string| {
-                list_box(&scrolled_window, songs.filter(artist.eq(artist_string)).select(album).group_by(album)
-                    .get_results::<Option<String>>(&mut get_connection()).unwrap(), or_none, {
+                list_box(&scrolled_window, songs.filter(artist.eq(&*artist_string)).select(album).group_by(album)
+                    .get_results::<Option<String>>(&mut get_connection()).unwrap()
+                    .into_iter().map(Rc::new).collect::<Vec<_>>(), vec![(Box::new(or_none), true)], {
                     let scrolled_window = scrolled_window.clone();
-                    let artist_string = artist_string.to_owned();
+                    let artist_string = artist_string.clone();
                     let media_controls = media_controls.clone();
                     move |album_string| {
                         list_box(&scrolled_window,
-                            songs.inner_join(collections).filter(artist.eq(&artist_string).and(album.eq(album_string)))
-                                .order_by(track_number).get_results::<(Song, Collection)>(&mut get_connection()).unwrap(),
-                            |(song, _)| {
-                                let artist_box = gtk_box(Horizontal);
-                                artist_box.append(&Label::new(song.track_number.map(|it| { it.to_string() }).as_deref()));
-                                artist_box.append(&Label::builder().hexpand(true).halign(Start).label(song.title.as_deref()
-                                    .unwrap_or(song.path.to_path().file_name().unwrap().to_str().unwrap())).build());
-                                artist_box.append(&Label::new(Some(&format(song.duration as u64))));
-                                artist_box
-                            }, {
+                            songs.inner_join(collections).filter(artist.eq(&*artist_string).and(album.eq(&*album_string)))
+                                .order_by(track_number).get_results::<(Song, Collection)>(&mut get_connection()).unwrap()
+                                .into_iter().map(Rc::new).collect::<Vec<_>>(),
+                            vec![
+                                (Box::new(|rc: Rc<(Song, Collection)>| {
+                                    let (song, _) = &*rc;
+                                    Label::new(song.track_number.map(|it| { it.to_string() }).as_deref())
+                                }) as Box<dyn Fn(Rc<(Song, Collection)>) -> Label>, false),
+                                (Box::new(|rc: Rc<(Song, Collection)>| {
+                                    let (song, _) = &*rc;
+                                    Label::builder().label(
+                                        song.title.as_deref().unwrap_or(song.path.to_path().file_name().unwrap()
+                                            .to_str().unwrap())
+                                    ).hexpand(true).xalign(0.0).max_width_chars(1).ellipsize(EllipsizeMode::End).build()
+                                }), true),
+                                (Box::new(|rc: Rc<(Song, Collection)>| {
+                                    let (song, _) = &*rc;
+                                    Label::new(Some(&format(song.duration as u64)))
+                                }), false),
+                            ], {
                                 let media_controls = media_controls.clone();
-                                move |(song, collection)| {
+                                move |rc| {
+                                    let (song, collection) = &*rc;
                                     media_controls.emit_by_name::<()>(SONG_SELECTED, &[&song.id, &song.path, &collection.path]);
                                 }
                             },
