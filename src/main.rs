f138d9410d3449888734b06d30758921a71d064f
dusk <ravenblackdusk@protonmail.com>
2023-07-14 09:33:04 +0330

    Use libadwaita

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use adw::{Application, ApplicationWindow, HeaderBar};
+use adw::prelude::{ApplicationExt, ApplicationExtManual, DisplayExt, SeatExt};
+use diesel::{ExpressionMethods, RunQueryDsl, update};
+use diesel::migration::Result;
+use diesel_migrations::MigrationHarness;
+use gtk::{Button, Label, MenuButton, Popover, ScrolledWindow};
+use gtk::Align::Fill;
+use gtk::gdk::SeatCapabilities;
+use gtk::glib::ExitCode;
+use gtk::Orientation::Vertical;
+use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, WidgetExt};
+use db::MIGRATIONS;
+use crate::collection::add_collection_box;
+use crate::common::constant::APP_ID;
+use crate::common::gtk_box;
+use crate::config::Config;
+use crate::controls::media_controls;
+use crate::db::get_connection;
+use crate::schema::config::{maximized, window_height, window_width};
+use crate::schema::config::dsl::config as config_table;
+
 mod schema;
 mod db;
 mod collection;
@@ -5,25 +26,6 @@ mod controls;
 mod common;
 mod config;
 mod home;
-
-use Align::Fill;
-use diesel::migration::Result;
-use diesel::{ExpressionMethods, RunQueryDsl, update};
-use diesel_migrations::MigrationHarness;
-use gtk::*;
-use gtk::gdk::SeatCapabilities;
-use gtk::prelude::*;
-use gtk::glib::ExitCode;
-use gtk::Orientation::Vertical;
-use db::MIGRATIONS;
-use crate::collection::add_collection_box;
-use crate::common::gtk_box;
-use crate::common::constant::APP_ID;
-use crate::config::Config;
-use crate::controls::media_controls;
-use crate::db::get_connection;
-use crate::schema::config::dsl::config as config_table;
-use crate::schema::config::{maximized, window_height, window_width};
 
 fn main() -> Result<ExitCode> {
     std_logger::Config::logfmt().init();
@@ -40,9 +42,10 @@ fn main() -> Result<ExitCode> {
         menu.append(&collection_button);
         let menu_button = MenuButton::builder().icon_name("open-menu-symbolic")
             .tooltip_text("Menu").popover(&Popover::builder().child(&menu).build()).build();
-        let main_box = Box::builder().spacing(4).orientation(Vertical).valign(Fill).build();
+        let main_box = gtk::Box::builder().spacing(4).orientation(Vertical).valign(Fill).build();
         let scrolled_window = ScrolledWindow::builder().vexpand(true).build();
         home::set_body(&scrolled_window, &media_controls);
+        main_box.append(&bar);
         main_box.append(&scrolled_window);
         main_box.append(&media_controls);
         collection_button.connect_clicked({
@@ -65,7 +68,7 @@ fn main() -> Result<ExitCode> {
         });
         bar.pack_start(&home_button);
         bar.pack_end(&menu_button);
-        let window_builder = ApplicationWindow::builder().application(application).child(&main_box).titlebar(&bar);
+        let window_builder = ApplicationWindow::builder().application(application).content(&main_box);
         let window = if !home_button.display().default_seat().unwrap().capabilities().contains(SeatCapabilities::TOUCH) {
             let config = config_table.get_result::<Config>(&mut get_connection()).unwrap();
             window_builder.default_width(config.window_width).default_height(config.window_height)
