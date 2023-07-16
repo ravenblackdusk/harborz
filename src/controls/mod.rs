2da0b65f3ec0a70cdd3049684f678eb85391b4c3
ghasemi <a.ghasemi@kian.digital>
2023-07-16 17:50:34 +0330

    Redesign now playing

--- src/controls/mod.rs
+++ src/controls/mod.rs
@@ -7,8 +7,7 @@ use gstreamer::glib::timeout_add_local;
 use gstreamer::MessageView::{AsyncDone, DurationChanged, StateChanged, StreamStart};
 use gstreamer::prelude::{Continue, ElementExt, ElementExtManual, ObjectExt};
 use gstreamer::State::{Null, Paused, Playing};
-use gtk::{Button, ContentFit, Grid, IconLookupFlags, IconTheme, Inhibit, Label, Picture, ProgressBar, Scale, ScrollType, TextDirection};
-use gtk::gdk::SeatCapabilities;
+use gtk::{Button, ContentFit, IconLookupFlags, IconTheme, Inhibit, Label, Picture, ProgressBar, Scale, ScrollType, TextDirection};
 use gtk::Orientation::{Horizontal, Vertical};
 use log::warn;
 use mpris_player::{Metadata, PlaybackStatus};
@@ -16,12 +15,11 @@ use util::format;
 use crate::collection::model::Collection;
 use crate::collection::song::{join_path, Song, WithCover};
 use crate::collection::song::WithPath;
-use crate::common::{BoldLabelBuilder, EllipsizedLabelBuilder, gtk_box, util};
+use crate::common::{BoldLabelBuilder, EllipsizedLabelBuilder, util};
 use crate::common::util::or_none;
 use crate::common::wrapper::{SONG_SELECTED, STREAM_STARTED, Wrapper};
 use crate::controls::mpris::mpris_player;
-use crate::controls::playbin::{go_delta_song, PLAYBIN, Playbin, URI};
-use crate::controls::volume::volume_button;
+use crate::controls::playbin::{PLAYBIN, Playbin, URI};
 use crate::db::get_connection;
 use crate::schema::collections::dsl::collections;
 use crate::schema::collections::path;
@@ -30,7 +28,6 @@ use crate::schema::config::dsl::config;
 use crate::schema::songs::dsl::songs;
 use crate::schema::songs::path as song_path;
 
-mod volume;
 mod playbin;
 mod mpris;
 
@@ -55,67 +52,35 @@ impl Playable for Button {
 
 pub fn media_controls() -> Wrapper {
     let mpris_player = mpris_player();
-    let now_playing = gtk::Box::builder().orientation(Horizontal).build();
-    let artist_album = gtk::Box::builder().orientation(Vertical).build();
+    let now_playing = gtk::Box::builder().orientation(Horizontal)
+        .margin_start(8).margin_end(8).margin_top(8).margin_bottom(8).build();
+    let song_info = gtk::Box::builder().orientation(Vertical).build();
     let unknown_album_file = IconTheme::for_display(&now_playing.display())
         .lookup_icon("audio-x-generic", &[], 128, 1, TextDirection::None, IconLookupFlags::empty()).file().unwrap();
     let album_picture = Picture::builder().content_fit(Contain).file(&unknown_album_file).build();
     now_playing.append(&album_picture);
-    now_playing.append(&artist_album);
-    let song_label = Label::builder().margin_ellipsized(4).bold().build();
-    let artist_label = Label::builder().margin_ellipsized(4).build();
-    artist_album.append(&song_label);
-    artist_album.append(&artist_label);
+    now_playing.append(&song_info);
     let play_pause = Button::builder().hexpand(true).build();
     play_pause.play();
+    now_playing.append(&play_pause);
+    let song_label = Label::builder().margin_ellipsized(4).bold().build();
+    let artist_label = Label::builder().margin_ellipsized(4).build();
+    song_info.append(&song_label);
+    song_info.append(&artist_label);
+    let time_box = gtk::Box::builder().spacing(4).margin_start(4).build();
     let position_label = Label::new(Some(&format(0)));
+    time_box.append(&position_label);
+    time_box.append(&Label::new(Some("/")));
+    let duration_label = Label::new(Some(&format(0)));
+    time_box.append(&duration_label);
+    song_info.append(&time_box);
     let scale = Scale::builder().hexpand(true).build();
     scale.set_range(0.0, 1.0);
-    let duration_label = Label::new(Some(&format(0)));
-    let controls_and_progress = gtk::Box::builder().orientation(Vertical).build();
+    let now_playing_and_progress = gtk::Box::builder().orientation(Vertical).build();
     let progress_bar = ProgressBar::new();
     progress_bar.add_css_class("osd");
-    controls_and_progress.append(&progress_bar);
-    let controls = gtk::Box::builder().orientation(Vertical).spacing(4)
-        .margin_start(8).margin_end(8).margin_top(8).margin_bottom(8).build();
-    controls_and_progress.append(&controls);
-    let position_box = gtk_box(Horizontal);
-    let control_grid = Grid::builder().build();
-    position_box.append(&position_label);
-    position_box.append(&scale);
-    position_box.append(&duration_label);
-    let skip_backward = Button::builder().icon_name("media-skip-backward").tooltip_text("Previous")
-        .hexpand(true).build();
-    skip_backward.connect_clicked(|_| { go_delta_song(-1, true); });
-    control_grid.attach(&skip_backward, 0, 0, 2, 1);
-    let seek_backward = Button::builder().icon_name("media-seek-backward").tooltip_text("Seek 10s backward")
-        .hexpand(true).build();
-    seek_backward.connect_clicked({
-        let position_label = position_label.clone();
-        let progress_bar = progress_bar.clone();
-        let scale = scale.clone();
-        move |_| { PLAYBIN.simple_seek(Duration::from_secs(10), false, &position_label, &progress_bar, &scale); }
-    });
-    control_grid.attach(&seek_backward, 2, 0, 1, 1);
-    control_grid.attach(&play_pause, 3, 0, 3, 1);
-    let seek_forward = Button::builder().icon_name("media-seek-forward").tooltip_text("Seek 30s forward")
-        .hexpand(true).build();
-    seek_forward.connect_clicked({
-        let position_label = position_label.clone();
-        let progress_bar = progress_bar.clone();
-        let scale = scale.clone();
-        move |_| { PLAYBIN.simple_seek(Duration::from_secs(30), true, &position_label, &progress_bar, &scale); }
-    });
-    control_grid.attach(&seek_forward, 6, 0, 1, 1);
-    let skip_forward = Button::builder().icon_name("media-skip-forward").tooltip_text("Next").hexpand(true).build();
-    skip_forward.connect_clicked(|_| { go_delta_song(1, true); });
-    control_grid.attach(&skip_forward, 7, 0, 2, 1);
-    if !control_grid.display().default_seat().unwrap().capabilities().contains(SeatCapabilities::TOUCH) {
-        control_grid.attach(&volume_button(|volume| { PLAYBIN.set_property("volume", volume); }), 9, 0, 1, 1);
-    }
-    controls.append(&now_playing);
-    controls.append(&position_box);
-    controls.append(&control_grid);
+    now_playing_and_progress.append(&progress_bar);
+    now_playing_and_progress.append(&now_playing);
     play_pause.connect_clicked(move |play_pause| {
         if PLAYBIN.current_state() == Playing {
             PLAYBIN.set_state(Paused).unwrap();
@@ -146,7 +111,7 @@ pub fn media_controls() -> Wrapper {
             Inhibit(true)
         }
     });
-    let wrapper = Wrapper::new(&controls_and_progress);
+    let wrapper = Wrapper::new(&now_playing_and_progress);
     wrapper.connect_local(SONG_SELECTED, true, {
         let play_pause = play_pause.clone();
         move |params| {
